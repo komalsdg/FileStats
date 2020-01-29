6fc7345ec6be5b846a3561d0eeea98ff01bc93e7
komalsdg <komalsdg@gmail.com>
2020-01-29 17:39:15 +0530

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FileStats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpFile = new System.Windows.Forms.GroupBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.txtFileSize = new System.Windows.Forms.TextBox();
+            this.lblFileSize = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.grpFile.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpFile
+            // 
+            this.grpFile.Controls.Add(this.btnFile);
+            this.grpFile.Controls.Add(this.txtFileSize);
+            this.grpFile.Controls.Add(this.lblFileSize);
+            this.grpFile.Controls.Add(this.txtFileName);
+            this.grpFile.Controls.Add(this.lblFileName);
+            this.grpFile.Location = new System.Drawing.Point(12, 12);
+            this.grpFile.Name = "grpFile";
+            this.grpFile.Size = new System.Drawing.Size(314, 127);
+            this.grpFile.TabIndex = 0;
+            this.grpFile.TabStop = false;
+            this.grpFile.Text = "File Details";
+            // 
+            // btnFile
+            // 
+            this.btnFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFile.Location = new System.Drawing.Point(231, 98);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 4;
+            this.btnFile.Text = "Select File...";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.BtnFile_Click);
+            // 
+            // txtFileSize
+            // 
+            this.txtFileSize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFileSize.Location = new System.Drawing.Point(70, 62);
+            this.txtFileSize.Name = "txtFileSize";
+            this.txtFileSize.ReadOnly = true;
+            this.txtFileSize.Size = new System.Drawing.Size(236, 20);
+            this.txtFileSize.TabIndex = 3;
+            // 
+            // lblFileSize
+            // 
+            this.lblFileSize.AutoSize = true;
+            this.lblFileSize.Location = new System.Drawing.Point(7, 64);
+            this.lblFileSize.Name = "lblFileSize";
+            this.lblFileSize.Size = new System.Drawing.Size(49, 13);
+            this.lblFileSize.TabIndex = 2;
+            this.lblFileSize.Text = "File Size:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFileName.Location = new System.Drawing.Point(70, 30);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(236, 20);
+            this.txtFileName.TabIndex = 1;
+            this.txtFileName.Text = "Select a File...";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(7, 32);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(57, 13);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "File Name:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 145);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 161);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Byte Counts";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(306, 129);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 313);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpFile.ResumeLayout(false);
+            this.grpFile.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpFile;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.TextBox txtFileSize;
+        private System.Windows.Forms.Label lblFileSize;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileStats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFile_Click(object sender, EventArgs e)
+        {
+            FileInfo fi = null;
+
+            listView1.View = View.Details;
+            listView1.Columns.Add("Byte Value");
+            listView1.Columns.Add("Count");
+
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Select a file";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fi = new FileInfo(dialog.FileName);
+                }
+                if (fi != null)
+                {
+                    if (fi.Length > 0)
+                    {
+                        txtFileName.Text = fi.FullName;
+                        txtFileSize.Text = Convert.ToString(fi.Length);
+                        FileRead(fi);
+                    }
+                    else
+                    {
+                        MessageBox.Show("File size should be at least one byte of size", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void FileRead(FileInfo fi)
+        {
+            Thread oThread = new Thread(() => FileProcess(fi));
+            oThread.Start();
+        }
+
+        private void FileProcess(FileInfo fi)
+        {
+            int byteCapacity = 2048;
+
+            FileStream fs = new FileStream(fi.FullName, FileMode.Open, System.IO.FileAccess.Read);
+
+            byte[] chunk = new byte[byteCapacity];
+            Queue<byte> fQueue = new Queue<byte>();
+
+            while (true)
+            {
+                int index = 0;
+                while (index < chunk.Length)
+                {
+                    int bytesRead = fs.Read(chunk, index, chunk.Length - index);
+                    if (bytesRead == 0)
+                        break;
+                    index += bytesRead;
+                }
+
+                if (index != 0)
+                {
+                    byte[] cbuffer = new byte[index];
+                    fs.Lock(0, byteCapacity);
+                    for (int i = 0; i < index; i++)
+                    {
+                        fQueue.Enqueue(chunk[i]);
+                    }
+                    fs.Unlock(0, byteCapacity);
+                }
+                if (index != byteCapacity)
+                {
+                    fs.Flush();
+                    fs.Close();
+                    ListByteCount(fQueue);
+                    return;
+                }
+            }
+        }
+
+        private void ListByteCount(Queue<byte> fQueue)
+        {
+            Dictionary<string, int> byteList = new Dictionary<string, int>();
+
+            byte[] byteArray = new byte[fQueue.Count];
+
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                string byt = fQueue.Dequeue().ToString("X2");
+
+                if (byteList.ContainsKey(byt))
+                {
+                    byteList[byt] = GetCount(byteList, byt) + 1;
+                }
+                else
+                    byteList[byt] = 1;
+            }
+
+            var listByteData = byteList.OrderByDescending(r => r.Value).ToList();
+
+
+            foreach (var item in listByteData)
+            {
+                Invoke(new MethodInvoker(
+                    delegate { listView1.Items.Add(new ListViewItem(new string[] { item.Key, item.Value.ToString() })); }
+                    ));
+            }
+        }
+
+        private int GetCount(Dictionary<string, int> byteList, string key)
+        {
+            int countVal;
+            byteList.TryGetValue(key, out countVal);
+            return countVal;
+        }
+    }
+}
